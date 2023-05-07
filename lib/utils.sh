asdf-vm/asdf-ruby

Bump ruby-build version to v20230428

lib/utils.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RUBY_BUILD_VERSION="${ASDF_RUBY_BUILD_VERSION:-v20230424}"
+RUBY_BUILD_VERSION="${ASDF_RUBY_BUILD_VERSION:-v20230428}"
 RUBY_BUILD_TAG="$RUBY_BUILD_VERSION"
 
 echoerr() {

mise-plugins/rtx-ruby

lib/utils.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RUBY_BUILD_VERSION="${RTX_RUBY_BUILD_VERSION:-v20230424}"
+RUBY_BUILD_VERSION="${RTX_RUBY_BUILD_VERSION:-v20230428}"
 RUBY_BUILD_TAG="$RUBY_BUILD_VERSION"
 
 echoerr() {